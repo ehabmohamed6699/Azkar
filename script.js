f78a8e10b6314e59e18a3fb2f8dae4c9d190661b
ehabmohamed6699 <ihabmohamed6699@gmail.com>
2025-02-23 00:26:05 +0200

    added reset button

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let selectedCategory = ""
-window.onload = () => {
-    fetch("adkar.json").then(res => res.json()).then((data) => {
-        let buttonsContainer = document.createElement("div")
+let initializeView = (data) => {
+    let buttonsContainer = document.createElement("div")
         let azkarContainer = document.createElement("div")
         buttonsContainer.classList.add("d-flex", "align-items-center", "flex-wrap", "gap-3", "my-4")
         azkarContainer.classList.add("d-flex", "flex-column", "gap-3")
@@ -17,9 +16,13 @@ window.onload = () => {
                 zekrBody.appendChild(zekr)
                 let zekrButton = document.createElement("button")
                 zekrButton.classList.add("btn", "btn-success")
+                if(item.count === 0){
+                    zekrButton.classList.add("disabled")
+                }
                 zekrButton.textContent = item.count
                 zekrButton.onclick = () => {
                     item.count--
+                    window.localStorage.setItem("azkar", JSON.stringify(data))
                     zekrButton.textContent = item.count
                     if(item.count === 0){
                         zekrButton.classList.add("disabled")
@@ -44,7 +47,27 @@ window.onload = () => {
             }
             buttonsContainer.appendChild(category)
         })
+        let resetButton = document.createElement("button")
+        resetButton.classList.add("btn", "btn-success")
+        resetButton.textContent = "إعادة الضبط"
+        resetButton.onclick = () => {
+            window.localStorage.removeItem("azkar")
+            window.location.reload()
+        }
+        resetButton.style.position = "fixed"
+        resetButton.style.bottom = "20px"
+        resetButton.style.right = "20px"
+        
         document.body.appendChild(buttonsContainer)
         document.body.appendChild(azkarContainer)
-    })
+        document.body.appendChild(resetButton)
+}
+window.onload = () => {
+    if(window.localStorage.getItem("azkar")){
+        initializeView(JSON.parse(window.localStorage.getItem("azkar")))
+    }else{
+        fetch("adkar.json").then(res => res.json()).then((data) => {
+            initializeView(data)
+        })
+    }
 }
